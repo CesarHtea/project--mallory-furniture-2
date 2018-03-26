75adb248edc1cb9f84fbf14da733f6eeb432d7ae
DamianAllende <amigosaburridos@gmail.com>
2018-03-25 21:26:31 -0600

    Import Header.js in App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './component/Header'
+
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Start Project</h1>
+        <Header />
       </div>
     );
   }

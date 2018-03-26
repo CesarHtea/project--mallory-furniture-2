b6a4e529f828c5587df3fe599b07ad2661de97c2
DamianAllende <amigosaburridos@gmail.com>
2018-03-26 00:53:48 -0600

    add className to Slide.js

--- src/component/Slide.js
+++ src/component/Slide.js
@@ -3,9 +3,9 @@ class Slide extends Component {
   render() {
     return (
       <section>
-        <div>
-          <h1>Morllory Furniture</h1>
-          <p>Your furniture is old<br/>Our is older</p>
+        <div className='slide__container'>
+          <h1 className='slide__title'>Morllory Furniture</h1>
+          <p className='slide__slogan'>Your furniture is old<br/>Our is older</p>
         </div>
       </section>
     );

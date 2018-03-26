c3184748baf79c9468cd4ecca8b465bb4b50b963
DamianAllende <amigosaburridos@gmail.com>
2018-03-26 00:46:32 -0600

    Import Slide.js to App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './css/styles.css';
 
 import Header from './component/Header'
+import Slide from './component/Slide'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <Slide />
       </div>
     );
   }

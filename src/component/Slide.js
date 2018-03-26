2b41c86245e1487061df39c64c266bfc1bfbf9dd
DamianAllende <amigosaburridos@gmail.com>
2018-03-26 01:34:06 -0600

    add div with className slide__texts

--- src/component/Slide.js
+++ src/component/Slide.js
@@ -4,8 +4,10 @@ class Slide extends Component {
     return (
       <section>
         <div className='slide__container'>
-          <h1 className='slide__title'>Morllory Furniture</h1>
-          <p className='slide__slogan'>Your furniture is old<br/>Our is older</p>
+          <div className='slide__texts'>
+            <h1 className='slide__title'>Morllory Furniture</h1>
+            <p className='slide__slogan'>Your furniture is old<br/>Our is older</p>
+          </div>
         </div>
       </section>
     );

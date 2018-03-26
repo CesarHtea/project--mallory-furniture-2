5fce8b45252fa7dc4c5b140e1ed08baf58529463
DamianAllende <amigosaburridos@gmail.com>
2018-03-26 00:43:06 -0600

    Create Slide.js and basic structure

---
+++ src/component/Slide.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+class Slide extends Component {
+  render() {
+    return (
+      <section>
+        <div>
+          <h1>Morllory Furniture</h1>
+          <p>Your furniture is old<br/>Our is older</p>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Slide;

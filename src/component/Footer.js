fa23b5e104986e0606bb0e4b03204180e72779a0
DamianAllende <amigosaburridos@gmail.com>
2018-03-26 12:57:36 -0600

    Add div className .footer__container to Footer.js

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -7,30 +7,32 @@ class Footer extends Component {
         <div className='footer__image'>
           <img src=''/>
         </div>
-        <section className='footer__section'>
-          <h3 className='footer__title'>Company</h3>
-          <ul className='footer__list'>
-            <li>About</li>
-            <li>Prees</li>
-            <li>Terms + Conditions</li>
-          </ul>
-        </section>
-        <section>
-          <h3 className='footer__title'>Categories</h3>
-          <ul className='footer__list'>
-            <li>Seating</li>
-            <li>Tables</li>
-            <li>Misc</li>
-          </ul>
-        </section>
-        <section>
-          <h3 className='footer__title'>Social</h3>
-          <ul className='footer__list'>
-            <li><i></i></li>
-            <li><i></i></li>
-            <li><i></i></li>
-          </ul>
-        </section>
+        <div className='footer__container'>
+          <section className='footer__section'>
+            <h3 className='footer__title'>Company</h3>
+            <ul className='footer__list'>
+              <li>About</li>
+              <li>Prees</li>
+              <li>Terms + Conditions</li>
+            </ul>
+          </section>
+          <section>
+            <h3 className='footer__title'>Categories</h3>
+            <ul className='footer__list'>
+              <li>Seating</li>
+              <li>Tables</li>
+              <li>Misc</li>
+            </ul>
+          </section>
+          <section>
+            <h3 className='footer__title'>Social</h3>
+            <ul className='footer__list'>
+              <li><i></i></li>
+              <li><i></i></li>
+              <li><i></i></li>
+            </ul>
+          </section>
+        </div>  
       </footer>
     );
   }

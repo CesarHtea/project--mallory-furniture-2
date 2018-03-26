ad8bce207eb007151a39480f3d1a604f704a30cd
DamianAllende <amigosaburridos@gmail.com>
2018-03-26 11:17:07 -0600

    Basic structure HTML Footer.js

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,7 +3,32 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return (
-      
+      <footer>
+        <section>
+          <h3>Company</h3>
+          <ul>
+            <li>About</li>
+            <li>Prees</li>
+            <li>Terms + Conditions</li>
+          </ul>
+        </section>
+        <section>
+          <h3>Categories</h3>
+          <ul>
+            <li>Seating</li>
+            <li>Tables</li>
+            <li>Misc</li>
+          </ul>
+        </section>
+        <section>
+          <h3>Social</h3>
+          <ul>
+            <li><i></i></li>
+            <li><i></i></li>
+            <li><i></i></li>
+          </ul>
+        </section>
+      </footer>
     );
   }
 }

0721767968e507469009c693673db4d73812dddc
DamianAllende <amigosaburridos@gmail.com>
2018-03-26 22:47:22 -0600

    Create component All.js, Basic structure, add tag html

---
+++ src/component/All.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class All extends Component {
+  render() {
+    return (
+    	<div>
+    		<h1>This is All</h1>
+    	</div>
+    );
+  }
+}
+
+export default All;

b055a9bb072a053440b2751944070594361b7e48
DamianAllende <amigosaburridos@gmail.com>
2018-03-26 22:34:16 -0600

    Add component Slide.js to Home.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Slide from './Slide'
 
 
 class Home extends Component {
   render() {
     return (
     	<div>
+		<Slide />
     		<h1>This is Home</h1>
     	</div>
     );

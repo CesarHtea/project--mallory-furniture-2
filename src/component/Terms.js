f0e87829a1c552bce1433cba68659ba151d8bd3f
DamianAllende <amigosaburridos@gmail.com>
2018-03-26 21:30:51 -0600

    Add className to Terms.js

--- src/component/Terms.js
+++ src/component/Terms.js
@@ -4,16 +4,16 @@ import React, { Component } from 'react';
 class Terms extends Component {
   render() {
     return (
-    	<div>
-    		<h1>Terms + Conditions</h1>
-    		<h2>Terms of Service</h2>
-    		<p>
+    	<div className='terms'>
+    		<h1 className='terms__title'>Terms + Conditions</h1>
+    		<h2 className='terms__subTitle'>Terms of Service</h2>
+    		<p className='terms__text'>
 				All buyers (hereinafter 'You' or 'User') need to register by creating a username and a password for login.
 				The information you provide needs to be accurate. You may not use false information or impersonate another person or company through your account.
 				You are responsible for maintaining the confidentiality of the login details and Your account. Artlimes reserves the right to reject Your subscription for any or no reason, in particular, due to User's breach of these Terms of Service.
 			</p>
-			<h2>Privacy Policy</h2>
-			<p>
+			<h2 className='terms__subTitle'>Privacy Policy</h2>
+			<p className='terms__subTitle'>
 				User’s personal data handling and protection is governed under the terms hereof and the provisions of General Data Protection Regulation (Regulation EC/679/2016).
 				We don’t share your personal information with anyone except to comply with the law, develop our products, or protect our rights.
 				Mallory Furniture collects non-personally-identifying information of the sort that web browsers and servers typically make available, such as the browser type, language preference, referring site, and the date and time of each visitor request. Mallory Furniture's purpose in collecting non-personally identifying information is to better understand how Mallory Furniture's visitors use its website. From time to time, Mallory Furniture may release non-personally-identifying information in the aggregate, e.g., by publishing a report on trends in the usage of its website.

892df7b386ede66ce8cc6835a6325b68b6dc9dcd
DamianAllende <amigosaburridos@gmail.com>
2018-03-26 19:06:37 -0600

    Create component Terms.js, create basic structure and add basic tag html

---
+++ src/component/Terms.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Terms extends Component {
+  render() {
+    return (
+    	<div>
+    		<h1>This is Terms</h1>
+    	</div>
+    );
+  }
+}
+
+export default Terms;

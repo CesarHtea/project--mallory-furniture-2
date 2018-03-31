6544ae4199924f66b7805f55418cfedd228a788a
DamianAllende <amigosaburridos@gmail.com>
2018-03-31 01:55:12 -0600

    Footer

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,37 +1,41 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Footer extends Component {
   render() {
     return (
       <footer className='footer'>
         <div className='footer__image'>
-          <img src=''/>
         </div>
         <div className='footer__container'>
-          <section className='footer__section'>
-            <h3 className='footer__title'>Company</h3>
-            <ul className='footer__list'>
-              <li>About</li>
-              <li>Prees</li>
-              <li>Terms + Conditions</li>
-            </ul>
-          </section>
-          <section className='footer__section'>
-            <h3 className='footer__title'>Categories</h3>
-            <ul className='footer__list'>
-              <li>Seating</li>
-              <li>Tables</li>
-              <li>Misc</li>
-            </ul>
-          </section>
-          <section className='footer__section'>
-            <h3 className='footer__title'>Social</h3>
-            <ul className='footer__list'>
-              <li><i className='fa fa-instagram'></i></li>
-              <li><i className='fa fa-twitter'></i></li>
-              <li><i className='fa fa-pinterest'></i></li>
-            </ul>
-          </section>
+          <div className='footer__container__map'>
+            <section className='footer__section'>
+              <h3 className='footer__title'>Company</h3>
+              <ul className='footer__list'>
+                <li><Link to={''} className='footer__list__link'>About</Link></li>
+                <li><Link to={''} className='footer__list__link'>Prees</Link></li>
+                <li><Link to={''} className='footer__list__link'>Terms + Conditions</Link></li>
+              </ul>
+            </section>
+            <section className='footer__section'>
+              <h3 className='footer__title'>Categories</h3>
+              <ul className='footer__list'>
+                <li><Link to={''} className='footer__list__link'>Seating</Link></li>
+                <li><Link to={''} className='footer__list__link'>Tables</Link></li>
+                <li><Link to={''} className='footer__list__link'>Misc</Link></li>
+              </ul>
+            </section>
+          </div> 
+          <div className='footer__container__social'> 
+            <section className='footer__section'>
+              <h3 className='footer__title'>Social</h3>
+              <ul className='footer__list'>
+                <li><i className='fa fa-instagram'></i></li>
+                <li><i className='fa fa-twitter'></i></li>
+                <li><i className='fa fa-pinterest'></i></li>
+              </ul>
+            </section>
+          </div>  
         </div>  
       </footer>
     );
